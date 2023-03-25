38b28eb73cb222d9b251a137b8a99f7ef21326f4
pandusanika <pandu.martinez24@gmail.com>
2023-03-25 19:05:52 +0700

    the view model

--- app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
@@ -5,30 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ubaya.advweek4.R
+import com.ubaya.advweek4.viewmodel.ListViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel:ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +26,42 @@ class StudentListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_student_list, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        val recView = view.findViewById<RecyclerView>(R.id.recView)
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            studentListAdapter.updateStudentList(it)
+        })
+
+        val txtError = view.findViewById<TextView>(R.id.txtError)
+        val recView = view.findViewById<RecyclerView>(R.id.recView)
+        val progressLoad = view.findViewById<ProgressBar>(R.id.progressLoad)
+
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                txtError.visibility = View.VISIBLE
+            } else {
+                txtError.visibility = View.GONE
             }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                recView.visibility = View.GONE
+                progressLoad.visibility = View.VISIBLE
+            } else {
+                recView.visibility = View.VISIBLE
+                progressLoad.visibility = View.GONE
+            }
+        })
     }
 }
